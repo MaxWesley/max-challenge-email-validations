f8203e07e738ac9da5b208814a8f5130c32a998b
MaxWesley <mx_wesley@hotmail.com>
2022-09-24 04:40:11 -0300

    feat: e-mail valid color

--- js/validateEmailInput.js
+++ js/validateEmailInput.js
@@ -1,6 +1,6 @@
 function validateEmailInput() {
   const inputEmail = document.querySelector("input");
-  const alertText = document.querySelector('#alert');
+  const alertText = document.getElementById('alert');
 
   const emailDigitado = inputEmail.value;
 
@@ -9,5 +9,9 @@ function validateEmailInput() {
 
   if(existeArroba === false || existePonto === false) {
     alertText.innerHTML = 'E-mail inválido. Verifique o e-mail e tente novamente.';
+    return;
   }
+
+  alertText.style.color = "#0bb83f";
+  alertText.innerHTML = 'E-mail é válido';
 }
